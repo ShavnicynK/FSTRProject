7b45338f9d0d4effb9f3de4fba631f0c6db52b9a
ShavnicynK <shavnicyn.k@yandex.ru>
2023-09-06 16:19:39 +0300

    add serializers for pereval

--- Pereval/app/serializers.py
+++ Pereval/app/serializers.py
@@ -28,3 +28,92 @@ class ImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Image
         fields = ('title', 'image')
+
+
+class PerevalAddedSerializer(serializers.ModelSerializer):
+    customuser = CustomUserSerializer()
+    coordinates = CoordinatesSerializer()
+    level = LevelSerializer()
+    image = ImageSerializer(many=True)
+
+    class Meta:
+        model = PerevalAdded
+        fields = (
+            'beautyTitle',
+            'title',
+            'other_titles',
+            'connect',
+            'add_time',
+            'status',
+            'coordinates',
+            'level',
+            'customuser',
+            'image'
+        )
+
+    def create(self, validated_data):
+        customuser_data = validated_data.pop('customuser')
+        customuser_email = customuser_data['email']
+        customuser_fam = customuser_data['fam']
+        customuser_name = customuser_data['name']
+        customuser_otch = customuser_data['otch']
+        customuser_phone = customuser_data['phone']
+        if CustomUser.object.filter(email=customuser_email).exist():
+            customuser_id = CustomUser.object.filter(email=customuser_email)[0]
+        else:
+            CustomUser.objects.create(
+                email=customuser_email,
+                fam=customuser_fam,
+                name=customuser_name,
+                otc=customuser_otch,
+                phone=customuser_phone
+            )
+            customuser_id = CustomUser.object.filter(email=customuser_email)[0]
+
+        coordinates_data = validated_data.pop('coordinates')
+        coordinates_latitude = coordinates_data['latitude']
+        coordinates_longitude = coordinates_data['longitude']
+        coordinates_height = coordinates_data['height']
+        Coordinates.objects.create(
+            latitude=coordinates_latitude,
+            longitude=coordinates_longitude,
+            height=coordinates_height
+        )
+        coordinates_id = Coordinates.object.filter(
+            latitude=coordinates_latitude,
+            longitude=coordinates_longitude,
+            height=coordinates_height
+        )[0]
+
+        level_data = validated_data.pop('level')
+        level_winter = level_data['winter']
+        level_summer = level_data['summer']
+        level_autumn = level_data['autumn']
+        level_spring = level_data['spting']
+        Level.objects.create(
+            winter=level_winter,
+            summer=level_summer,
+            autumn=level_autumn,
+            spring=level_spring
+        )
+        level_id = Level.object.filter(
+            winter=level_winter,
+            summer=level_summer,
+            autumn=level_autumn,
+            spring=level_spring
+        )[0]
+
+        validated_data.setdefault('customuser', customuser_id)
+        validated_data.setdefault('coordinates', coordinates_id)
+        validated_data.setdefault('level', level_id)
+        cur_pereval = PerevalAdded.objects.create(**validated_data)
+
+        image_data = validated_data.pop('image')
+        for image in image_data:
+            cur_image, status = Image.objects.get_or_create(**image)
+            PerevalImage.objects.create(image=cur_image, pereval=cur_pereval)
+
+        return cur_pereval
+
+
+
