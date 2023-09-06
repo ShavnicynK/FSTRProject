0226dace3ff88d0b63d58f83e27bdd10d331fbb2
ShavnicynK <shavnicyn.k@yandex.ru>
2023-09-06 13:11:37 +0300

    add model CustomUser

--- Pereval/app/models.py
+++ Pereval/app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
-# Create your models here.
+
+phone_valid = RegexValidator(
+        regex=r'^\+?1?\d{9,12}$',
+        message="Phone number must be entered in the format: '+999999999'. Up to 12 digits allowed.")
+
+
+class CustomUser(models.Model):
+    email = models.CharField(max_length=200, unique=True, blank=False)
+    name = models.CharField(max_length=50, blank=True)
+    fam = models.CharField(max_length=50, blank=True)
+    otch = models.CharField(max_length=50, blank=True)
+    phone = models.CharField(validators=[phone_valid], max_length=12, blank=True)
+
+    def __str__(self):
+        return f'{self.email} {self.fam} {self.fam} {self.otch} {self.phone}'
