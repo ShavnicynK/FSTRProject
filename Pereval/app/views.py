77647fe1d7af92f68fbc50ad446bd2f9992a2d6c
ShavnicynK <shavnicyn.k@yandex.ru>
2023-09-06 16:25:47 +0300

    add viewsets for all

--- Pereval/app/views.py
+++ Pereval/app/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from .models import *
+from .serializers import *
+from rest_framework import viewsets
 
-# Create your views here.
+
+class PerevalAddedViewSet(viewsets.ModelViewSet):
+    queryset = PerevalAdded.objects.all()
+    serializer_class = PerevalAddedSerializer
+
+
+class CustomUserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+
+
+class CoordinatesViewSet(viewsets.ModelViewSet):
+    queryset = Coordinates.objects.all()
+    serializer_class = CoordinatesSerializer
+
+
+class CustomuserViewSet(viewsets.ModelViewSet):
+    queryset = Level.objects.all()
+    serializer_class = LevelSerializer
+
+
+class ImageViewSet(viewsets.ModelViewSet):
+    queryset = Image.objects.all()
+    serializer_class = ImageSerializer
+
