5cb7818dcc72e4fde4c85cec9d5d3e41f96d1217
ShavnicynK <shavnicyn.k@yandex.ru>
2023-09-06 16:32:09 +0300

    add router

--- Pereval/Pereval/urls.py
+++ Pereval/Pereval/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from app.views import PerevalAddedViewSet, CustomUserViewSet, CoordinatesViewSet, LevelViewSet, ImageViewSet
+
+router = DefaultRouter()
+router.register('submitData', PerevalAddedViewSet)
+router.register('users', CustomUserViewSet)
+router.register('coordinates', CoordinatesViewSet)
+router.register('level', LevelViewSet)
+router.register('image', ImageViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
